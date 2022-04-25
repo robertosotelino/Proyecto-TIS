17c0274a0748384435c9f6c099d3d2b17db4675d
Roberto Sotelino Martínez <r.sotelino@usp.ceu.es>
2022-04-25 14:19:45 +0200

    Update Menu

--- RIPFashion/src/ui/Menu.java
+++ RIPFashion/src/ui/Menu.java
@@ -5,7 +5,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.time.format.DateTimeFormatter;
-
+import java.util.ArrayList;
 import db.interfaces.DBManager;
 import db.jdbc.JDBCManager;
 
@@ -13,13 +13,13 @@ import db.jdbc.JDBCManager;
 
 public class Menu {
 
-	private static JDBCManager dbman = new JDBCManager(); //??
+	private static DBManager dbman = new JDBCManager(); 
 	
 	
 	private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	private final static DateTimeFormatter formatterFecha = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm");
 	private static final String[] MENU_ROL = { "1.-Rol Empresario", "2.-Rol Cliente","3.-Salir del programa"};
-	private static final String[] MENU_EMPRESARIO = { "Consultar informacion de la tienda","Listar Articulos", 
+	private static final String[] MENU_EMPRESARIO = { "1.-Consultar informacion de la tienda","Listar Articulos", 
 			"Consultar capital","Consultar articulo por id", "Añadir un articulo",
 			"Eliminar un articulo","Modificar articulo","Añadir marca","Añadir una tienda","Añadir un empleado",
 			"Listar empleados", "Salir"};
@@ -50,20 +50,75 @@ public class Menu {
 		
 	
 		}
+
+	private static void mostrarMenuEmpresario() {
+		
+		System.out.println("/n/n*****MENU DEL EMPRESARIO*****/n/n");
+		
+		int respuesta = -1;
+		
+		do {
+			respuesta = mostrarMenu(MENU_EMPRESARIO);
+			
+			switch(respuesta) {
+			
+				case 1 -> consultarInformacion();
+			
+			}
+			
+		} while(respuesta != 0);
+		
+	}
+
+	private static void consultarInformacion() {
+
+		int i ;
+		ArrayList<Tienda> tiendas = dbman.getInfoTiendasConCapital();
+		
+		for ( i = 0 ; i < dbman.getInfoTiendasConCapital().size() ; i++) {
+			
+			System.out.println(dbman.getInfoTiendasConCapital());
+			
+		}
+		
+	}
+
+	private static int mostrarMenu(String[] opciones) {
+		int respuesta = -1;
+		do {
+			System.out.println("\nElija una opción:");
+			for(int i = 1; i < opciones.length; i++) {
+				System.out.println(i + ". " + opciones[i]);
+			}
+			System.out.println("0. " + opciones[0]);
+			try {
+				respuesta = Integer.parseInt(br.readLine());
+			} catch (IOException e) {
+				
+			} catch (NumberFormatException e) {
+				
+			}
+			if(respuesta < 0 || respuesta >= opciones.length) {
+				System.out.println("Por favor introduzca un número válido");
+			}
+		} while(respuesta < 0 || respuesta >= opciones.length);
+		return respuesta;
+		
+	}
 	
-	private static void mostrarMenuEmpresario() throws IOException { // INCOMPLETO
+	/*private static void mostrarMenuEmpresario() throws IOException { // INCOMPLETO
 		
 		System.out.println(MENU_EMPRESARIO);
 		
 		int a = -1;
-		//int b = 0;  // ????
 
 		do {
 			
 			int opcion = br.read();
 			int b ; 
 			
-            switch (opcion) {
+            switch (opcion) { // en cada case un metodo privado que hago en esta clase
+            
             
             case 1 :
             	System.out.println(dbman.getInfoTiendasConCapital()); // consulto informacion de la tienda
@@ -96,7 +151,7 @@ public class Menu {
 		}while (a != 0);
 		
 	}
-
+*/
 
 	}
 
