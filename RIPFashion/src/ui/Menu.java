039d30daca9cc6d41cf6d437147b9364dd6188b4
Roberto Sotelino Martínez <r.sotelino@usp.ceu.es>
2022-04-25 10:09:47 +0200

    Update Menu

--- RIPFashion/src/ui/Menu.java
+++ RIPFashion/src/ui/Menu.java
@@ -10,12 +10,15 @@ import db.jdbc.JDBCManager;
 
 public class Menu {
 
-	//private static DBManager dbman = new JDBCManager();
+	private static JDBCManager dbman = new JDBCManager();
 	
 	private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	private final static DateTimeFormatter formatterFecha = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm");
 	private static final String[] MENU_ROL = { "1.-Rol Empresario", "2.-Rol Cliente","3.-Salir del programa"};
-	private static final String[] MENU_EMPRESARIO = { "Listar Articulos", "Buscar articulo", "Listar empleadps", "Salir"};
+	private static final String[] MENU_EMPRESARIO = { "Consultar informacion de la tienda","Listar Articulos", 
+			"Consultar capital","Consultar articulo por id", "Añadir un articulo",
+			"Eliminar un articulo","Modificar articulo","Añadir marca","Añadir una tienda","Añadir un empleado",
+			"Listar empleados", "Salir"};
 
 	public static void main (String[] args) throws IOException {
 		
@@ -44,15 +47,33 @@ public class Menu {
 	
 		}
 	
-	private static void mostrarMenuEmpresario() throws IOException {
+	private static void mostrarMenuEmpresario() throws IOException { // INCOMPLETO
 		
 		System.out.println(MENU_EMPRESARIO);
 		
 		int a = -1;
-		
+		int b = 0;  // ????
+
 		do {
+			
 			int opcion = br.read();
 			
+            switch (opcion) {
+            
+            case 1 -> System.out.println(dbman.getInfoTiendasConCapital()); // consulto informacion de la tienda
+			case 2 -> dbman.getArticulos(); // consultar articulos
+			case 3 -> dbman.consultarBalance(null); // consultar capital  -> Como le paso la tienda?
+			case 4 -> System.out.println(dbman.searchArticuloByIdArt( b = br.read())); // consultar la informacion de un articulo determinado   
+			case 5 -> dbman.addArticulo(null);  // añadir un articulo
+			case 6 -> dbman.deleteArticuloById(0); // eliminar un articulo determinado
+			case 7 -> dbman.updateArticulo(null); // modificar un articulo existente
+			case 8 -> dbman.addMarca(null); // añadir una marca			
+			case 9 -> dbman.addTienda(null); // añadir una tienda
+			case 10 -> dbman.addEmpleado(null); // añadir un empleado
+			case 11 -> dbman.getEmpleados(); // listar empleados
+			case 12 -> a = 0; // salir
+			
+			}
 			
 		}while (a != 0);
 		
