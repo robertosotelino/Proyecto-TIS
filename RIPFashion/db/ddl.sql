384c359178d42cb35df6d31cc93b527fc34d9a68
Roberto Sotelino Martínez <r.sotelino@usp.ceu.es>
2022-06-02 20:00:21 +0200

    Update ddl.sql

--- RIPFashion/db/ddl.sql
+++ RIPFashion/db/ddl.sql
@@ -9,9 +9,9 @@ PRIMARY KEY (NombreTienda)
 
 );
 
-INSERT INTO Tiendas VALUES ("Delfin&Maria", 10000000,"10:00-20:00", "Rua Paseo 19", "Calzado y complementos")
-INSERT INTO Tiendas VALUES ("Sterling", 10000000,"10:00-20:00", "Rua San Miguel 7", "Ropa de mujer")
-INSERT INTO Tiendas VALUES ("Delfin 1953", 10000000,"10:00-20:00", "Rua San Miguel 11", "Calzado de caballero")
+INSERT INTO Tiendas VALUES ("Delfin&Maria", 10000000,"10:00-20:00", "Rua Paseo 19", "Calzado y complementos");
+INSERT INTO Tiendas VALUES ("Sterling", 10000000,"10:00-20:00", "Rua San Miguel 7", "Ropa de mujer");
+INSERT INTO Tiendas VALUES ("Delfin 1953", 10000000,"10:00-20:00", "Rua San Miguel 11", "Calzado de caballero");
 
 CREATE TABLE IF NOT EXISTS Clientes (
 
